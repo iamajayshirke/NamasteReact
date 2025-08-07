13af7ea36f74d560735b46d7f6f0a6381b7415d8
iamajayshirke <ajayshirke137@gmail.com>
2025-08-07 22:18:14 +0530

    React Component Basics

--- react.js
+++ react.js
@@ -1,8 +1,18 @@
-import React from "react"
-import * as ReactDOM from "react-dom/client"
+import React from "react";
+import * as ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1",{},"Hello World, ReactJS")
+// const heading = React.createElement("h1",{},"Hello World, ReactJS")
+// const jsxHeading = <h1>React Js</h1>
+const title = () => <h2>15 Days Fast Course🚀</h2>;
+const ReactComp = () => {
+  return (
+    <>
+      <h1>Welcome to ReactJS</h1>
+      {title()}
+    </>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading)
+root.render(<ReactComp />);
