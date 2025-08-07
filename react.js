164a3f59ddb7f9b3251b7871b0d68b58211b30d9
iamajayshirke <ajayshirke137@gmail.com>
2025-08-07 16:28:03 +0530

    `Added ReactJS functionality to index.html and created a basic React script in react.js`

--- react.js
+++ react.js
@@ -1,1 +1,8 @@
-console.log("Namaste React")
+import React from "react"
+import * as ReactDOM from "react-dom/client"
+
+const heading = React.createElement("h1",{},"Hello World, ReactJS")
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading)
