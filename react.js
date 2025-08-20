bc6a58121d45e85f84c2265207e4fa9e914eb923
iamajayshirke <ajayshirke137@gmail.com>
2025-08-20 13:25:32 +0530

    `feat: updated index.html, package.json and react.js files with title change and added Navbar and Restaurant components`

--- react.js
+++ react.js
@@ -1,14 +1,14 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
+import Navbar from "./src/components/Navbar";
+import Restaurant from "./src/components/Restaurant";
 
-// const heading = React.createElement("h1",{},"Hello World, ReactJS")
-// const jsxHeading = <h1>React Js</h1>
 const title = () => <h2>15 Days Fast Course🚀</h2>;
 const ReactComp = () => {
   return (
     <>
-      <h1>Welcome to ReactJS</h1>
-      {title()}
+      <Navbar/>
+      <Restaurant />
     </>
   );
 };
