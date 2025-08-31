a251c08f9fa96db31c2d3f28726b28a32488d58d
iamajayshirke <ajayshirke137@gmail.com>
2025-09-01 00:54:26 +0530

    `Refactor RestoCardWithLabel HOC: update styles and layout`

--- src/utils/HOC.js
+++ src/utils/HOC.js
@@ -1,10 +1,14 @@
 export const RestoCardWithLabel = (RestoCard) => {
-    return (props) => {
-      return (
-        <>
-          <div className="absolute bg-blue-950 text-amber-50 z-3 rounded-md p-1">Customers Choice</div>
+  return (props) => {
+    return (
+      <>
+        <div className="relative">
+          <div className="absolute bg-blue-950 text-amber-50 z-3 rounded-md p-1 left-[-1rem] top-[-1rem]">
+            Customers Choice
+          </div>
           <RestoCard {...props} />
-        </>
-      );
-    };
+        </div>
+      </>
+    );
   };
+};
